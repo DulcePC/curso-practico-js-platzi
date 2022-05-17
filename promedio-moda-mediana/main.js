0f7ca2487d81d676553b206b4687c36ca3c698ab
Dulce Perez <d.perez@kation.com.do>
2022-05-17 17:27:12 -0400

    Feat :sparkles: add mediana function

--- promedio-moda-mediana/main.js
+++ promedio-moda-mediana/main.js
@@ -1,38 +1,18 @@
 //Mediana
-function calcularMedia(lista) {
-  // const sumaLista = lista.reduce(
-  //   function ( valorAcumulado = 0, nuevoElemento) {
-  //     return valorAcumulado + nuevoElemento;
-  //   }
-  // );
-  // const promedioLista = sumaLista / lista.length;
-  // return promedioLista;
-}
-
-//Test
-const lista1 = [
-  100,
-  400,
-  10,
-  800
-]
-
-const mitadLista1  = parseInt(lista1.length / 2);
-let mediana;
-
-function esPar(numerito) {
-  if(numerito % 2 === 0) {
-    return true;
+function getMediana(list) {
+  list.sort(function(a,b){return a - b});
+  let middleItem = parseInt(list.length / 2);
+  if (list.length % 2 === 0) {
+    const firstElement = list[middleItem];
+    const secondElement = list[middleItem - 1];
+    return console.log((firstElement + secondElement) / 2);
   } else {
-    return false;
+    return console.log(list[middleItem]);
   }
 }
-
-if(esPar(lista1.length)) {
-  const elemento1 = lista1[mitadLista1];
-  const elemento2 = lista1[mitadLista1 - 1];
-  const promedioElemento1y2 = calcularMedia([elemento1,elemento2]);
-  media = promedioElemento1y2;
-} else {
-  mediana = lista1[mitadLista1];
-}
+getMediana([100,
+  200,
+  500,
+  400000000,
+  10,
+  90]);
